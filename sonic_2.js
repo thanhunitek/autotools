f0bce7b1b5f333de5681f230461e87e89102ccb1
thanhunitek <86224332+thanhunitek@users.noreply.github.com>
2024-10-20 08:58:00 +0700

    Update sonic_2.js

--- sonic_2.js
+++ sonic_2.js
@@ -1,21 +1,10 @@
-// XPaths of the elements
-var step1Xpath = '//*[@id=":r10:-form-item"]/div/div[1]/div/img';
+// CSS selector for the button in Step 1 (replacing XPath)
+var step1Selector = 'button#\\:rs\\:-form-item';  // The new selector for the button element
+
 // CSS selector for the button in Step 3
 var step3Selector = 'button[type="submit"].wr-bg-green-500';  // Selector for the green button
 
-// Function to click an element by XPath
-function clickElementByXPath(xpath) {
-  var element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  
-  if (element) {
-    element.click();
-    console.log("Element clicked (XPath): " + xpath);
-  } else {
-    console.log("Element not found (XPath): " + xpath);
-  }
-}
-
-// Function to click an element by a CSS selector
+// Function to click an element by CSS selector
 function clickElementBySelector(selector) {
   var element = document.querySelector(selector);
   
@@ -34,8 +23,8 @@ let interval = setInterval(() => {
     clearInterval(interval);
     console.log("Finished 20 loops.");
   } else {
-    // Step 1: Click the first element using XPath
-    clickElementByXPath(step1Xpath);
+    // Step 1: Click the first element using the new CSS selector
+    clickElementBySelector(step1Selector);
 
     // Step 2: Wait 10 seconds (handled by the interval)
     setTimeout(() => {
@@ -46,4 +35,3 @@ let interval = setInterval(() => {
     loopCount++;
   }
 }, 20000);
-
